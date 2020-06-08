6d3fb646be5571ea84a9f06003e98b88e10a49c4
dmahajan980 <divyanshumahajan98@gmail.com>
2020-06-08 15:58:31 +0530

    Fixed linting errors and added configuration blocks to Gruntfile.js and package-lock.json to .gitignore

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,15 @@
+/* eslint-env node */
 "use strict";
 
 module.exports = function (grunt) {
     grunt.config.init({
         lintAll: {
             sources: {
-                js:    ["./src/js/**/*.js", "tests/js/**/*.js", "./*.js", "!./src/lib/**/*.js"]
+                js:    ["./src/js/**/*.js", "tests/js/**/*.js", "./*.js", "!./src/lib/**/*.js"],
+                md:    [ "./*.md", "tests/**/*.md", "!tests/fixtures/md/bad.md"],
+                json:  ["./*.json", "./.*.json", "tests/**/*.json", "!tests/fixtures/json/bad.json"],
+                json5: ["./*.json5", "tests/**/*.json5", "!tests/fixtures/json5/bad.json5", "!tests/fixtures/json5/dangling-comma.json5"],
+                other: ["./.*"]
             }
         }
     });
