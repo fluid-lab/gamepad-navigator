83986d5377badd230a319ea3080b851b8acf7f23
dmahajan980 <divyanshumahajan98@gmail.com>
2020-06-08 18:47:24 +0530

    Removed excluded references from the Gruntfile configuration blocks and pinned devDependencies to their exact versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,10 @@ module.exports = function (grunt) {
     grunt.config.init({
         lintAll: {
             sources: {
-                js:    ["./src/js/**/*.js", "tests/js/**/*.js", "./*.js", "!./src/lib/**/*.js"],
-                md:    [ "./*.md", "tests/**/*.md", "!tests/fixtures/md/bad.md"],
-                json:  ["./*.json", "./.*.json", "tests/**/*.json", "!tests/fixtures/json/bad.json"],
-                json5: ["./*.json5", "tests/**/*.json5", "!tests/fixtures/json5/bad.json5", "!tests/fixtures/json5/dangling-comma.json5"],
+                js:    ["./src/js/**/*.js", "tests/js/**/*.js", "./*.js"],
+                md:    [ "./*.md", "tests/**/*.md"],
+                json:  ["./*.json", "./.*.json", "tests/**/*.json"],
+                json5: ["./*.json5", "tests/**/*.json5"],
                 other: ["./.*"]
             }
         }
